a82dc0e6ebf609ec0f7add23fb2a55e3da74a41d
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-07-11 19:47:38 +0400

    feat: add main page in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-// import { useState } from 'react'
-import './styles/search.css'
 import './App.css'
+import MainPage from './pages/MainPage'
 
 function App() {
   return (
     <>
-      <h1>What do you want to find?</h1>
-      <div className="search-bar">
-        <input className="search-input" placeholder="ex.: bananas"></input>
-        <button className="search-button">Search</button>
-      </div>
-      <div className="results-block">
-        <div className="result-card"></div>
-      </div>
+      <MainPage></MainPage>
     </>
   )
 }
