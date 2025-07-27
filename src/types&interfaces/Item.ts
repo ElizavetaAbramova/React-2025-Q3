f76155f8f1a8d39c2350df46e5831ea6c2f616e3
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-07-27 17:35:30 +0400

    refactor: create folder for types and interfaces

---
+++ src/types&interfaces/Item.ts
@@ -0,0 +1,9 @@
+export type Item = {
+  id: number
+  title: string
+  description: string
+  images: string[]
+  availabilityStatus: string
+  brand: string
+  price: number
+}
---
+++ src/types&interfaces/Status.ts
@@ -0,0 +1,1 @@
+export type Status = 'error' | 'loading' | 'fulfilled' | 'empty'
