6541cc660c8a788c5b4af3e25fc9d38647933fd9
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-07-27 17:39:21 +0400

    fix: add new function for api requests

--- src/api/getItems.ts
+++ src/api/getItems.ts
@@ -1,13 +1,15 @@
-export default async function getItems(param: string) {
+export default async function getItems(param: string, skip = 0) {
+  const limit = 10
   const query = encodeURIComponent(param)
   const url =
     param === ''
-      ? 'https://dummyjson.com/products'
-      : `https://dummyjson.com/products/search?q=${query}`
+      ? `https://dummyjson.com/products/search?limit=${limit}&skip=${skip}`
+      : `https://dummyjson.com/products/search?q=${query}&limit=${limit}&skip=${skip}`
   const response = await fetch(url)
   if (!response.ok) {
     throw new Error(`Failed to load data. Error code: ${response.status}`)
   }
   const jsonObj = await response.json()
-  return jsonObj.products
+
+  return { list: jsonObj.products, total: jsonObj.total, currentPage: jsonObj.skip + 1 }
 }
